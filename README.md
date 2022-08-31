c20109b8e59e06f0d1193e611cffc7fc57b3ebb2
Maarten Wolzak <mwolzak@magnatron.nl>
2022-08-31 14:30:51 +0200

    Initial commit is always first

---
+++ README.md
@@ -0,0 +1,20 @@
+# Quietfield
+## A less distracting Stackfield / Electron app
+
+[Stackfield](https://www.stackfield.com) is a great project management app! One of the few really GDPR compliant ones, fully built, maintained and hosted in Germany, Europe.
+
+A downside is the many (WAAAYYYYY TOO MUCH) distracting in-app notifications.
+
+*Quietfield* is a simple Electron / Forge configuration whichc adds some simple CSS to quiet down the multitude of notifications. Enabling you to work without being alerted every-single-time a co-worker does anything-at-all -not-related-to-you in a room you are member of.
+
+### Install
+
+Make sure you have installed [Electron](https://www.electronjs.org/docs/latest/tutorial/quick-start) and [Electron Forge](https://www.electronforge.io/).
+
+```
+git clone https://github.com/MaartenW/Quietfield.git
+cd Quietfield
+npm run make
+```
+
+Enjoy!
---
+++ src/main.js
@@ -0,0 +1,32 @@
+// Inject into site:
+var extraCSS = [""
+    ,"#DivTopBar, #DivCookiesMessageNew, #DivFooterBarWrapper { display:none; }"
+    // Do not display intrusive "New Activities" banner in sidebar:
+    ,".DivNewActivityOverlay { margin-left: -100%; }"
+    // Do not display activities that are not related to you:
+    , ".SpnGroupUnreadedMarker { visibility: hidden; }"
+];
+
+const { app, BrowserView, BrowserWindow } = require('electron')
+
+app.whenReady().then(() => {
+    const win = new BrowserWindow({ width: 1200, height: 800 })
+  
+    const view = new BrowserView()
+    win.setBrowserView(view)
+    view.setBounds({ x: 0, y: 0, width: 1200, height: 772 })
+    view.setAutoResize({width:true, height:true})
+
+    view.webContents.addListener('did-navigate', (e,u,c,t)=>{
+        view.webContents.insertCSS(extraCSS.join(" ")).then(result => {
+            console.log('CSS Added Successfully')
+            console.log('Unique Key Returned ', result)
+            cssKey = result;
+        }).catch(error => {
+            console.log(error);
+        });
+    });
+
+    view.webContents.loadURL('https://www.stackfield.com/login.htm')
+
+})
